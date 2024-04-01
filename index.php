a986182897b4b9eba3952db58843151c29c0e199
Vadym Zakrevskyy <vadym.zakrevskyy@gmail.com>
2024-03-31 20:22:17 -0400

    Added fancontrol.ini

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    phpinfo();
+    $ini = parse_ini_file('fancontrol.ini');
 
-    $output = shell_exec('ls -lart');
-    echo "<pre>$output</pre>";
+    //$output = shell_exec('ls -lart');
+    echo "<pre>";
+    echo $ini['sendook'];
+    echo "</pre>";
 ?>
