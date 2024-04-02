589dfe2672982caf5d57690abaa76309867c8e75
root <vadym.zakrevskyy@gmail.com>
2024-04-01 22:28:59 -0400

    Dev updates

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
+<pre>
+
 <?php
     $ini = parse_ini_file('fancontrol.ini');
 
-    echo "<pre>";
-    echo $ini['sendook'];
-    echo "</pre>";
-
-    $output = shell_exec($ini['sendook'] . ' -h');
-    echo $output;
+    $t = 'sudo '.$ini['sendook'].' -f '.$ini['frequency'].' -0 '.$ini['duration_0'].' -1 '.$ini['duration_1'].' -p '.$ini['pause'].' '.$ini['office'].$ini['light'];
+    echo $t;
+    $output = shell_exec($t);
+    //print_r($output);
+    echo $output; 
 ?>
+
+</pre>
