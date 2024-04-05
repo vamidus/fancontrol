e7b5df9d0f2e509eeaf3c47c92e4231660149c33
root <vadym.zakrevskyy@gmail.com>
2024-04-04 23:06:33 -0400

    Added carousel functionality

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,39 +1,69 @@
 class Main {
 	constructor() {
-		// Configs
-		// Variables
-		// Selectors
+		this.$areaButtons = [];
+		this.$areaCarousel = null;
 	}
 	initialize(settings) {
 		if (settings) this.applySettings(settings);
+		this.setActivePage();
 		this.setup();
 	}
 	applySettings(settings) {
 		$.extend(this, settings);
 	}
 	setup() {
-		this.setupButtonEventHandler();
+		this.setupElementSelectors();
+		this.setupButtonsEventHandler();
+		this.setupCarouselEventHandler();
 	}
-	setupButtonEventHandler() {
+	setupElementSelectors() {
+		this.$areaButtons = $("button[data-area]");
+		this.$areaCarousel = $("#areaCarousel");
+	}
+	setupButtonsEventHandler() {
 		const me = this;
-		$("button").on("click", me.buttonEventHandler.bind(me));
+		this.$areaButtons.on("click", me.buttonsEventHandler.bind(me));
 	}
-	buttonEventHandler(event) {
+	buttonsEventHandler(event) {
+		navigator.vibrate([1]);
 		const $target = $(event.target);
 		const area = $target.attr("data-area");
 		const instruction = $target.attr("data-instruction");
-		// console.log(`area: ${area}`);
-		// console.log(`instruction: ${instruction}`);
 		$.ajax({
 			url: "fancontrol.php",
 			type: "post",
 			dataType: 'json',
-			data: { area, instruction },
-			success: function(result) {
-				console.log(result);
-			}
+			data: { area, instruction }
 		});
 	}
+	setupCarouselEventHandler() {
+		const me = this;
+		this.$areaCarousel.on("slid.bs.carousel", me.carouselEventHandler.bind(me));
+	}
+	carouselEventHandler() {
+		const area = $("div.active[data-area]").attr("data-area");
+		this.setCookie("area", area, 90);
+	}
+	setActivePage() {
+		const area = this.getCookie("area");
+		if (area) {
+			$("div.carousel-item[data-area]").removeClass("active");
+			$(`div.carousel-item[data-area="${area}"]`).addClass("active");
+		}
+	}
+	setCookie(key, value, expiry) {
+        let expires = new Date();
+        expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+        document.cookie = `${key}=${value};expires=${expires.toUTCString()}`;
+    }
+    getCookie(key) {
+        const keyValue = document.cookie.match(`(^|;) ?${key}=([^;]*)(;|$)`);
+        return keyValue ? keyValue[2] : null;
+    }
+    eraseCookie(key) {
+        const keyValue = getCookie(key);
+        setCookie(key, keyValue, '-1');
+    }
 	static CreateInstance(settings) {
 		const instance = new Main();
 		instance.initialize(settings);
--- fancontrol.php
+++ fancontrol.php
@@ -1,18 +1,20 @@
 <?php
 	$area = $_POST['area'];
 	$instruction = $_POST['instruction'];
-	if(!empty($area) && !empty($instruction)) {
-		sendook($area, $instruction);
-	}
+	sendook($area, $instruction);
 
 	function sendook($area, $instruction) {
 		$ini = parse_ini_file('fancontrol.ini');
+		if (empty($ini[$area]) || empty($ini[$instruction])) {
+			http_response_code(400);
+			exit();
+		}
 		$command = 'sudo '.$ini['sendook']
 			.' -f '.$ini['frequency']
 			.' -0 '.$ini['duration_0']
 			.' -1 '.$ini['duration_1']
 			.' -p '.$ini['pause']
-			.' '.$ini[$area].$ini[$instruction];
+			.' -d '.$ini[$area].$ini[$instruction];
 		shell_exec($command);
 	}
 ?>
